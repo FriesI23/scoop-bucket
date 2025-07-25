9489bf4b5e011ba7a35a8f59f6812d89bb806426
Fries_I23 <FriesI23@outlook.com>
2025-07-25 21:06:04 +0800

    chore: init project

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "FriesI23/scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
